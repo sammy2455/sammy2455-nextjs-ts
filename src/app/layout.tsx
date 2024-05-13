5af5bce0ea09699a0bda65045ac68daebe651e4a
sammy2455 <sami-camaton@hotmail.com>
2024-05-12 20:05:21 -0500

    Fixed problem with de script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,13 +47,13 @@ export default function RootLayout({
           <Footer />
         </div>
 
-        <script src="/assets/js/vendor/jquery-3.6.0.min.js"></script>
-        <script src="/assets/js/bootstrap.bundle.min.js"></script>
-        <script src="/assets/js/jquery.magnific-popup.min.js"></script>
-        <script src="/assets/js/ajax-form.js"></script>
-        <script src="/assets/js/clipboard.min.js"></script>
-        <script src="/assets/js/slick.min.js"></script>
-        <script src="/assets/js/script.js"></script>
+        <Script src="/assets/js/vendor/jquery-3.6.0.min.js" strategy="beforeInteractive"></Script>
+        <Script src="/assets/js/bootstrap.bundle.min.js" strategy="beforeInteractive"></Script>
+        <Script src="/assets/js/jquery.magnific-popup.min.js" strategy="beforeInteractive"></Script>
+        <Script src="/assets/js/ajax-form.js" strategy="beforeInteractive"></Script>
+        <Script src="/assets/js/clipboard.min.js" strategy="beforeInteractive"></Script>
+        <Script src="/assets/js/slick.min.js" strategy="beforeInteractive"></Script>
+        <Script src="/assets/js/script.js" strategy="beforeInteractive"></Script>
       </body>
     </html>
   );
